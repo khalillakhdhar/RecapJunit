e971d751e0e63c60cc131e3715d25b3d1e86755b
khalil <khalillakhdharatc@gmail.com>
2022-03-08 09:32:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RecapJunit
+ 
---
+++ src/testing/TestExemple.java
@@ -0,0 +1,16 @@
+package testing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestExemple {
+
+@Test // annotation appel d'une configuration
+public void testEqual()
+{
+	
+}
+	
+	
+	
+}
